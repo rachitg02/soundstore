2559de45832d2a2dc995e48dad0d8e8c95548560
Rachit Gupta <94759785+rachitg02@users.noreply.github.com>
2022-05-19 13:07:55 +0530

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import {BsBagCheckFill} from 'react-icons/bs';
 import {useStateContext} from '../context/StateContext';
 import { runFireworks } from '../lib/utils';
-const success = () => {
+const Success = () => {
     const {setCartItems, setTotalPrice, setTotalQuantities} = useStateContext();
     const [order, setOrder] = useState(null);
     useEffect(()=>{
